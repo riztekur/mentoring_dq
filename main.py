13f91e5720209166e71556835c0dee0594728c58
riztekur <riztekur@gmail.com>
2024-04-26 20:44:31 +0700

    menambahkan env variable

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+kode_api = os.environ("API_KEY")
 
 st.title("HELLO WORLD!")
 
 st.write("Helo nama saya rizki")
+st.write(kode_api)
